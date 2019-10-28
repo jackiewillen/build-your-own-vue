b4e0d5ecb324251f8cdf2c75f4e4fb3f1abb654a
jackieyin <jackieyin@tencent.com>
2019-10-28 16:33:34 +0800

    完成对于v-model的补足

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 任务清单：
 
-1. 实现一个变量，使控制台中对变量进行修改能够监听到
+1. 实现input和变量的双向绑定
    For example:
 
    HTML代码:
    <div>
         {{name}}
+        <input type="text" v-model="name">
    </div>
 
    JS代码：
@@ -18,4 +19,4 @@
    window.vm = vm; // 挂载到window上去
 
    Console控制台：
-   vm.name = 'willen'; // 改变会自动同步到页面上
+   vm.name = 'willen'; // 改变会自动同步到各个组件上去
--- js/Compile.js
+++ js/Compile.js
@@ -5,26 +5,69 @@ class Compile {
         if(this.$el) {
             // 在$fragment中操作，比this.$el中操作节省很多性能，所以要赋值给fragment
             let $fragment = this.node2Fragment(this.$el); // 将vue模板的地方使用片段替代，这是为了便于在内存中操作
-            this.compileText($fragment.childNodes[0]); // 将模板中的{{}}替换成对应的变量
+            this.compile($fragment);
             this.$el.appendChild($fragment); // 将替换好的片段追加到vue模板中去
         }
     }
+    
     node2Fragment(el) {
         // 将node节点都放到fragment中去
         var fragment  = document.createDocumentFragment();
-        fragment.appendChild(el.firstChild);// 将el中的元素放到fragment中去,并删除el中原有的，这个是appendChild自带的功能
+        var child;
+         // 将原生节点拷贝到fragment
+         while (child = el.firstChild) {
+            fragment.appendChild(child);
+        }
         return fragment;
     }
 
-    compileText(node) {
+    compile(el) {
+        var childNodes = el.childNodes;
+        var that = this;
+        childNodes.forEach(node => {
+            var text = node.textContent;
+            var reg = /\{\{(.*)\}\}/;
+            if(node.nodeType === 1) {
+                // 表示当前组件依然是一个html元素
+                that.compileElement(node);
+            } else if (node.nodeType === 3 && reg.test(text)) {
+                // 当前为一个文本节点时，并且文本节点中包含{{}}
+                that.compileText(node, RegExp.$1);
+            }
+        });
+    }
+
+    compileElement(node) {
+        // compile使用来对html元素进行编译，因为html上可能被添加了一些vue的属性
+        var nodeAttrs = node.attributes;
+        var that = this;
+        // Element.attributes 属性返回该元素所有属性节点的一个实时集合。该集合是一个 NamedNodeMap 对象，不是一个数组，所以它没有 数组 的方法，其包含的 属性 节点的索引顺序随浏览器不同而不同。更确切地说，attributes 是字符串形式的名/值对，每一对名/值对对应一个属性节点。所以这边不能够使用attributes.foreach
+        [].slice.call(nodeAttrs).forEach(attr => {
+            var attrName = attr.name;// 得到v-model
+            if(attrName.indexOf('v-') == 0) {
+                // 当为自定义的控件指令时
+                var exp = attr.value; // 得到v-model = "name"中的model
+                var val = that.$vue[exp]; // 得到name在$vue中存放的值
+                node.value = val; // 绑定值到input上去
+                node.addEventListener('input', (e) => {
+                    var newValue = e.target.value;
+                    if (val === newValue) {
+                        return;
+                    }
+                    that.$vue[exp] = newValue;
+                });
+                node.removeAttribute(attrName);
+            } else {
+
+            }
+        })
+    }
+
+    compileText(node, matchedName) {
         // 对包含可能出现vue标识的部分进行编译，主要是将{{xxx}}翻译成对应的值
-        var reg = /\{\{(.*)\}\}/; // 用来判断有没有vue的双括号的
-        if(reg.test(node.textContent)) {
-            let matchedName = RegExp.$1;
-            node.textContent = this.$vue[matchedName];
-            new Watch(this.$vue, matchedName, function(value) {
-                node.textContent = value;
-            });
-        }
+        node.textContent = this.$vue[matchedName];
+        new Watch(this.$vue, matchedName, function(value) {
+            node.textContent = value;
+        });
     }
 }
